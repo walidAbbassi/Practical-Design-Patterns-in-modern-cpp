19456a7763b00a1faa96587fc2f4051c03f290ca
abassi_ <walid.abbassi@esprit.tn>
2019-02-21 22:54:18 +0100

    add prototype design patterns creational

---
+++ Creational/prototype/Prototype.cpp
@@ -0,0 +1,79 @@
+/*
+* C++ Design Patterns: Prototype
+* Author: walid Abbassi [https://github.com/walidAbbassi]
+* 2019
+*
+* Source code is licensed under MIT License
+* (for more details see LICENSE)
+*
+*/
+
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <memory>
+
+/*
+* Animal  ==>  Prototype
+* declares an interface for cloning itself
+*/
+class PrototypeAnimal {
+public:
+	virtual std::shared_ptr<PrototypeAnimal> clone() = 0;
+	virtual std::string getType() const = 0;
+	// ...
+};
+
+/*
+* Concrete Prototype Sheep
+* implement an operation for cloning itself
+*/
+class ConcretePrototypeSheep : public PrototypeAnimal {
+public:
+	std::shared_ptr<PrototypeAnimal> clone() override {
+		return std::make_shared<ConcretePrototypeSheep>(*this);
+	}
+	std::string getType() const override {
+		return " Sheep ";
+	}
+	// ...
+};
+
+/*
+* Concrete Prototype Mouse
+* implement an operation for cloning itself
+*/
+class ConcretePrototypeMouse : public PrototypeAnimal {
+public:
+	std::shared_ptr<PrototypeAnimal > clone() override {
+		return std::make_shared<ConcretePrototypeMouse>(*this);
+	}
+	std::string getType() const override {
+		return " Mouse ";
+	}
+	// ...
+};
+
+/*
+* Client
+* creates a new object by asking a prototype to clone itself
+*/
+class ClientCrazyScientist {
+public:
+	static std::shared_ptr<PrototypeAnimal> cloningMachine(const std::shared_ptr<PrototypeAnimal> prototypeAnimal) {
+		return prototypeAnimal->clone();
+	}
+	// ...
+
+};
+
+
+int main()
+{
+	static std::shared_ptr<ConcretePrototypeSheep> Sheep = std::make_shared<ConcretePrototypeSheep>();
+	std::shared_ptr<PrototypeAnimal> animal = ClientCrazyScientist::cloningMachine(Sheep);
+	std::cout << "Prototype animal : " << animal->getType() << std::endl;
+
+	system("pause");
+	return 0;
+}
---
+++ Creational/prototype/README.md
@@ -0,0 +1,25 @@
+## Prototype
+
+Specify the kinds of objects to create using a prototypical instance, and create 
+new objects by copying this prototype. Pattern has creational purpose and deals 
+with object relationships, which are more dynamic. The pattern hides the complexities
+of making new instances from the client. 
+
+
+### UML class diagram example for the Factory Method Design Pattern.
+
+ - Client - creates a new object by asking a prototype to clone itself.
+ - Prototype - declares an interface for cloning itself.
+ - ConcretePrototype - implements the operation for cloning itself.
+
+ 
+<p align="center">
+<img 
+src="https://github.com/walidAbbassi/Practical-Design-Patterns-in-modern-cpp/blob/master/Creational/Factory%20Method/FactoryMethod.PNG">
+</p>
+
+### When to use
+
+* when the classes to instantiate are specified at run-time
+* to avoid building a class hierarchy of factories that parallels the class hierarchy of products
+* when instances of a class can have one of only a few different combinations of state 

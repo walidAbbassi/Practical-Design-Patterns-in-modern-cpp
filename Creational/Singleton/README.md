0cba460cd94a7fb7c86804ec3f15c45684531240
abassi_ <walid.abbassi@esprit.tn>
2019-02-21 23:59:58 +0100

    add Singleton design patterns creational

---
+++ Creational/Singleton/README.md
@@ -0,0 +1,22 @@
+## Singleton
+
+Ensure a class only has one instance, and provide a global point of access to it.
+Pattern has creational purpose and deals with object relationships, which are more 
+dynamic.
+
+
+### UML class diagram example for the Singleton Design Pattern.
+
+ - The Singleton Pattern defines a getInstance operation which exposes the unique instance which is accessed by the clients. 
+ - getInstance() - is is responsible for creating its class unique instance in case it is not created yet and to return that instance.
+
+ 
+<p align="center">
+<img 
+src="https://github.com/walidAbbassi/Practical-Design-Patterns-in-modern-cpp/blob/master/Creational/Factory%20Method/FactoryMethod.PNG">
+</p>
+
+### When to use
+
+* there must be exactly one instance of a class, and it must be accessible to clients from a well-known access point
+* when the sole instance should be extensible by subclassing, and clients should be able to use an extended instance without modifying their code
---
+++ Creational/Singleton/Singleton.cpp
@@ -0,0 +1,56 @@
+/*
+* C++ Design Patterns: Singleton
+* Author: walid Abbassi [https://github.com/walidAbbassi]
+* 2019
+*
+* Source code is licensed under MIT License
+* (for more details see LICENSE)
+*
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include <memory>
+#include <mutex>
+
+/*
+* Singleton
+* has private static variable to hold one instance of the class
+* and method which gives us a way to instantiate the class
+*/
+class Singleton {
+public:
+	static void NofityInit()
+	{
+		std::cout << "Initializing Singleton" << std::endl;
+	}
+
+	static Singleton &getInstance() {
+		std::call_once(Singleton::onceFlag, []() {
+			NofityInit();
+			instance.reset(new Singleton());
+		});
+		std::cout << "Getting  Singleton instance" << std::endl;
+		return *(instance.get());
+	}
+
+private:
+	static std::unique_ptr<Singleton> instance;
+	static std::once_flag onceFlag;
+
+	Singleton() = default;
+	Singleton(const Singleton& instance) = delete;
+	Singleton &operator=(const Singleton& instance) = delete;
+};
+
+std::unique_ptr<Singleton> Singleton::instance;
+std::once_flag Singleton::onceFlag;
+
+int main()
+{
+	Singleton& s1 = Singleton::getInstance();
+	Singleton& s2 = Singleton::getInstance();
+
+	system("pause");
+	return 0;
+}

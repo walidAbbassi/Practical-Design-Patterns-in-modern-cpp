cefecaec837a1e1bcefeab575306ca22dfbc1ea4
abassi_ <walid.abbassi@esprit.tn>
2019-02-25 20:12:40 +0100

    Create Skeleton_Bridge.cpp

---
+++ Structural/Bridge/Skeleton_Bridge.cpp
@@ -0,0 +1,115 @@
+/*
+* C++ Design Patterns: Bridge
+* Author: walid Abbassi [https://github.com/walidAbbassi]
+* 2019
+*
+* Source code is licensed under MIT License
+* (for more details see LICENSE)
+*
+*/
+
+#include <iostream>
+#include <memory>
+
+/*
+* Implementor
+* defines the interface for implementation classes
+*/
+class Implementor {
+public:
+	virtual void operationImpl(double side) = 0;
+	virtual ~Implementor(){}
+	// ...
+};
+
+/*
+* Concrete Implementors A
+* implement the Implementor interface and define concrete implementations
+*/
+class ConcreteImplementors_A : public Implementor {
+public:
+	void operationImpl(double side) override
+	{
+		std::cout << "Concrete Implementor A " << std::endl;
+	}
+	~ConcreteImplementors_A() {}
+	// ...
+};
+
+/*
+* Concrete Implementors B
+* implement the Implementor interface and define concrete implementations
+*/
+class ConcreteImplementors_B : public Implementor {
+public:
+	void operationImpl(double side) override
+	{
+		std::cout << "Concrete Implementor B " << std::endl;
+	}
+	~ConcreteImplementors_B() {}
+	// ...
+};
+
+/*
+* Abstraction
+* defines the abstraction's interface
+*/
+class Abstraction {
+public:
+	virtual void operation() = 0; // low-level
+	virtual void action(double size) = 0; // high-level
+	virtual ~Abstraction() {}
+protected:
+	std::weak_ptr<Implementor> implementor_ptr;
+	Abstraction(std::weak_ptr<Implementor> implementor) : implementor_ptr(implementor) {}
+	// ...
+};
+
+/*
+* RefinedAbstraction
+* extends the interface defined by Abstraction
+*/
+class RefinedAbstraction : public Abstraction {
+public:
+	RefinedAbstraction(double side, std::shared_ptr<Implementor> implementor)
+		: side(side), Abstraction(implementor) {}
+
+	//	 low-level i.e. Implementation specific
+	void operation() override
+	{
+		implementor_ptr.lock()->operationImpl(side);
+	}
+
+	//	 high-level i.e. Abstraction specific
+	void action(double size) override
+	{
+		std::cout << "action  " << std::endl;
+		side *= size;
+	}
+
+	~RefinedAbstraction() {}
+	// ...
+
+private:
+	double side;
+	double size;
+};
+
+
+
+int main()
+{
+	std::shared_ptr<Implementor> implementor_A = std::make_shared<ConcreteImplementors_A>();
+	std::shared_ptr<Implementor> implementor_B = std::make_shared<ConcreteImplementors_B>();
+
+	std::shared_ptr<Abstraction> abstraction = std::make_shared<RefinedAbstraction>(5, implementor_A);
+	abstraction->operation();
+	abstraction->action(2);
+	abstraction->operation();
+
+	abstraction = std::make_shared<RefinedAbstraction>(10, implementor_B);
+	abstraction->operation();
+
+	system("pause");
+	return 0;
+}

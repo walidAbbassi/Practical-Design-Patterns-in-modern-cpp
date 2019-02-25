7c92bb07c3804a95e3abbceb493c5f08e224cc06
abassi_ <walid.abbassi@esprit.tn>
2019-02-25 03:37:30 +0100

    add bridge dsign patterns structural

---
+++ Structural/Bridge/Bridge.cpp
@@ -0,0 +1,117 @@
+/*
+* C++ Design Patterns: Bridge
+* Author: walid Abbassi [https://github.com/walidAbbassi]
+* 2019
+*
+* Source code is licensed under MIT License
+* (for more details see LICENSE)
+*
+*/
+
+#include <iostream>
+#include <memory>
+
+/*
+* Drawing  ==>  Implementor
+* defines the interface for implementation classes
+*/
+class Drawing {
+public:
+	virtual void drawSquare(double side) = 0;
+	// ...
+};
+
+/*
+* DrawingSquarePencil  ==>  Concrete Implementors A 
+* implement the Implementor interface and define concrete implementations
+*/
+class DrawingSquareWithPencil : public Drawing {
+public:
+	void drawSquare(double side) override 
+	{
+		// use your function or use framework to draw : 
+		// openFrameworks(https://github.com/openframeworks/openFrameworks) /opencv/Qt/openGL ...
+		std::cout << "Concrete Implementor A draw Square with Pencil " << std::endl;
+	}
+	// ...
+};
+
+/*
+* DrawingRectangle  ==>  Concrete Implementors B
+* implement the Implementor interface and define concrete implementations
+*/
+class DrawingSquareWithBrush : public Drawing {
+public:
+	void drawSquare(double side) override 
+	{
+		// use your function or use framework to draw : 
+		// openFrameworks(https://github.com/openframeworks/openFrameworks) /opencv/Qt/openGL ...
+		std::cout << "Concrete Implementor B draw Square with Brush " << std::endl;
+	}
+	// ...
+};
+
+/*
+* Shape  ==>  Abstraction
+* defines the abstraction's interface
+*/
+class Shape {
+public:
+	virtual void draw() = 0; // low-level
+	virtual void resize(double size) = 0; // high-level
+	virtual ~Shape() {
+	}
+protected:
+	std::weak_ptr<Drawing> drawing;
+	Shape(std::weak_ptr<Drawing> drawing) : drawing(drawing) {}
+	// ...
+};
+
+/*
+* Square  ==>  RefinedAbstraction
+* extends the interface defined by Abstraction
+*/
+class Square : public Shape {
+public:
+	Square(double side, std::shared_ptr<Drawing> drawingImplementor)
+		: side(side), Shape(drawingImplementor) {}
+
+//	 low-level i.e. Implementation specific
+	void draw() override 
+	{
+		drawing.lock()->drawSquare(side);
+	}
+
+//	 high-level i.e. Abstraction specific
+	void resize(double size) override 
+	{
+		std::cout << "Resize Square with = " << size << std::endl;
+		side *= size;
+	}
+
+	~Square() {}
+	// ...
+
+private:
+	double side;
+	double size;
+};
+
+
+
+int main()
+{
+	std::shared_ptr<Drawing> drawBrush = std::make_shared<DrawingSquareWithBrush>();
+	std::shared_ptr<Drawing> drawPencil = std::make_shared<DrawingSquareWithPencil>();
+
+	std::shared_ptr<Shape> square = std::make_shared<Square>(5, drawBrush);
+	square->draw();
+	square->resize(2);
+	square->draw();
+
+	square = std::make_shared<Square>(10, drawPencil);
+	square->draw();
+
+	system("pause");
+	return 0;
+}
---
+++ Structural/Bridge/README.md
@@ -0,0 +1,25 @@
+## Bridge
+
+Decouple an abstraction from its implementation so that the two can vary independently. 
+Bridge pattern has structural purpose and applies to objects, so it deals with the composition of objects. 
+
+
+### UML class diagram example for the Bridge Design Pattern.
+
+ - Abstraction - Abstraction defines abstraction interface.
+ - AbstractionImpl - Implements the abstraction interface using a reference to an object of type Implementor.
+ - Implementor - Implementor defines the interface for implementation classes. This interface does not need to correspond directly to abstraction interface and can be very different. Abstraction imp provides an implementation in terms of operations provided by Implementor interface.
+ - ConcreteImplementor1, ConcreteImplementor2 - Implements the Implementor interface.
+
+ 
+<p align="center">
+<img 
+src="https://github.com/walidAbbassi/Practical-Design-Patterns-in-modern-cpp/blob/master/Structural/Adapter/Adapter.PNG">
+</p>
+
+### When to use
+
+* you want to avoid a permanent binding between an abstraction and its implementation
+* both the abstractions and their implementations should be extensible by subclassing
+* changes in the implementation of an abstraction should have no impact on clients
+* you want to hide the implementation of an abstraction completely from clients
